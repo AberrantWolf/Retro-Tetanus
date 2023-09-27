5c8afab777d28aef108574bf89d19fd205edeffe
Scott Harper <AberrantWolf@users.noreply.github.com>
2023-09-27 22:47:34 +0900

    Trying out some button and global callback things

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 slint::include_modules!();
 
 fn main() {
-    RetroTetanus::new().unwrap().run().unwrap()
+    let app = RetroTetanus::new().unwrap();
+
+    let weak_app = app.as_weak();
+    app.global::<Sorting>().on_get_source_path(move || {
+        let strong_app = weak_app.unwrap();
+        strong_app
+            .global::<Sorting>()
+            .set_source_path("C:\\Some\\Source\\Path".into());
+    });
+
+    let weak_app = app.as_weak();
+    app.global::<Sorting>().on_get_dest_path(move || {
+        let strong_app = weak_app.unwrap();
+        strong_app
+            .global::<Sorting>()
+            .set_dest_path("C:\\Some\\Dest\\Path".into());
+    });
+
+    app.run().unwrap()
 }
