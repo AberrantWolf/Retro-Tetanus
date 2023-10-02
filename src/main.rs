66e6c8db48a9e007fcb1a1c5aa0540662f505c4a
Scott Harper <AberrantWolf@users.noreply.github.com>
2023-10-02 23:12:25 +0900

    Tried out doing an async to get a dest path

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 slint::include_modules!();
 
-fn main() {
+use std::fs::File;
+
+use rfd::FileDialog;
+
+#[tokio::main]
+async fn main() {
     let app = RetroTetanus::new().unwrap();
 
     let weak_app = app.as_weak();
@@ -13,10 +18,24 @@ fn main() {
 
     let weak_app = app.as_weak();
     app.global::<Sorting>().on_get_dest_path(move || {
-        let strong_app = weak_app.unwrap();
-        strong_app
-            .global::<Sorting>()
-            .set_dest_path("C:\\Some\\Dest\\Path".into());
+        let weak_app = weak_app.clone();
+        std::thread::spawn(move || {
+            let file = FileDialog::new().pick_folder();
+            if file.is_none() {
+                return;
+            }
+
+            let file_path = file.unwrap().to_str().unwrap().to_owned();
+            let app_handle = weak_app.clone();
+
+            slint::invoke_from_event_loop(move || {
+                let strong_app = app_handle.unwrap();
+                strong_app
+                    .global::<Sorting>()
+                    .set_dest_path(file_path.into());
+            })
+            .expect("Error invoking main loop update");
+        });
     });
 
     app.run().unwrap()
