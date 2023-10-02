58f41a203b3469a6fee38c55db85d015cba21ef3
Scott Harper <AberrantWolf@users.noreply.github.com>
2023-10-02 23:48:46 +0900

    Doing tokio more better, maybe

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,41 @@
 slint::include_modules!();
 
-use std::fs::File;
-
-use rfd::FileDialog;
+use rfd::AsyncFileDialog;
 
 #[tokio::main]
 async fn main() {
     let app = RetroTetanus::new().unwrap();
 
-    let weak_app = app.as_weak();
+    let app_handle = app.as_weak();
     app.global::<Sorting>().on_get_source_path(move || {
-        let strong_app = weak_app.unwrap();
-        strong_app
-            .global::<Sorting>()
-            .set_source_path("C:\\Some\\Source\\Path".into());
-    });
-
-    let weak_app = app.as_weak();
-    app.global::<Sorting>().on_get_dest_path(move || {
-        let weak_app = weak_app.clone();
-        std::thread::spawn(move || {
-            let file = FileDialog::new().pick_folder();
+        let app_handle = app_handle.clone();
+        tokio::spawn(async move {
+            let file = AsyncFileDialog::new().pick_folder().await;
             if file.is_none() {
                 return;
             }
 
-            let file_path = file.unwrap().to_str().unwrap().to_owned();
-            let app_handle = weak_app.clone();
+            let file_path = file.unwrap().path().to_str().unwrap().to_owned();
+            slint::invoke_from_event_loop(move || {
+                let strong_app = app_handle.unwrap();
+                strong_app
+                    .global::<Sorting>()
+                    .set_source_path(file_path.into());
+            })
+            .expect("Error invoking main loop update");
+        });
+    });
 
+    let app_handle = app.as_weak();
+    app.global::<Sorting>().on_get_dest_path(move || {
+        let app_handle = app_handle.clone();
+        tokio::spawn(async move {
+            let file = AsyncFileDialog::new().pick_folder().await;
+            if file.is_none() {
+                return;
+            }
+
+            let file_path = file.unwrap().path().to_str().unwrap().to_owned();
             slint::invoke_from_event_loop(move || {
                 let strong_app = app_handle.unwrap();
                 strong_app
